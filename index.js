3c94b3e2883da1f19e29ac21653b8b0b291f9fec
Ummar Farooq <farooqcherur@gmail.com>
2023-08-06 12:07:44 +0530

    some changes

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 import puppeteer from "puppeteer";
 
 (async () => {
-  const browser = await puppeteer.launch({headless:false});
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto("https:/example.com");
-  await page.waitForSelector('input._3704LK');
-  const searchBox = await page.$('input._3704LK');
-  const searchTerm = 'iphone'
-  await searchBox.type(searchTerm);
-  await searchBox.press('Enter');
-  await page.waitForNavigation();
+
+  //   await page.waitForSelector('input._3704LK');
+  //   const searchBox = await page.$('input._3704LK');
+  //   const searchTerm = 'iphone'
+  //   await searchBox.type(searchTerm);
+  //   await searchBox.press('Enter');
+
+  await page.goto("http://localhost:3000/login");
+
+  await page.waitForSelector('input[name="Email"]');
+  
+  await page.waitForSelector('input[name="Password"]');
+
+  const email = "mymail@gmail.com";
+
+  const password = "123456";
+
+  await page.type('input[name="Email"]', email);
+
+  await page.type('input[name="Password"]', password);
+
+  await page.keyboard.press("Enter");
+
+  await page.goto("http://localhost:3000/products/Ice%20creams");
+
   await page.screenshot({ path: `./images/image.png`, fullPage: true });
+
   await page.pdf({ format: "A4", path: "./pdf/example.pdf" });
+
   await browser.close();
 })();
