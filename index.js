8203be1680343f8d069ddab3ecfbe0c1aaffd1c6
Ummar Farooq <farooqcherur@gmail.com>
2023-08-06 12:13:29 +0530

    some changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import puppeteer from "puppeteer";
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   //   await page.waitForSelector('input._3704LK');
